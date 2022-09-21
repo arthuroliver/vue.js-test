5297ffae576cc6e128227b0366c4bf7ddaab8c7a
arthuroliver <arthur1606@gmail.com>
2022-09-21 02:35:22 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Preço: <span>{{preco}}</span></p>
+    <p>Itens: <span>{{item}}</span></p>
+    <p>Preço Total: <span>{{item * preco}}</span></p>
+    <button @click="item++">Adicionar</button>
+    <button @click="item--">Remover</button>
+    <p>{{item ? 'Item adicionado' : 'Nenhum item adicionado'}}</p>
+    
+    <p>Message is: {{ message }} {{ checkbox }}</p>
+    <input v-model="message" placeholder="edit me">
+
+    <input type="checkbox" v-model="checkbox">
+
+
+    <div>Picked: {{picked}}</div>
+    <input type="radio" id="one" value="One" v-model="picked">
+    <label for="one">One</label>
+    <input type="radio" id="two" value="Two" v-model="picked">
+    <label for="two">Two</label>
+
+    <div>Selected: {{ selected }}</div>
+    <select v-model="selected">
+      <option disabled value="">Select one option</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+
+    <br>
+    <br>
+    <p>-------------------------------</p>
+    <br>
+    <br>
+
+    <select v-model="choice">
+      <option v-for="option in options" v-bind:key="option" :value="option.value">
+        {{option.text}}
+      </option>
+    </select>
+
+    <div>Selected: {{choice}}</div>
   </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      preco: 49,
+      item: 0,
+      message: "",
+      checkbox: "",
+      picked: "",
+      selected: "",
+      choice: "One",
+      options: [
+        { text: "first", value: "One" },
+        { text: "second", value: "Two" }
+      ]
+    }
   }
-}
+};
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
